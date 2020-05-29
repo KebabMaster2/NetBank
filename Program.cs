a3a0fdcb3c42f780d442a2345d3ba196eaa55231
Valdis <valpivars@gmail.com>
2020-05-29 17:08:17 +0300

    v1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
-namespace BankaBaltija
+namespace iBankaBaltija
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Account account = new Account("Yuri", 200);
+            Console.WriteLine($"Client {account.Owner} has {account.Balance} rubles in account number {account.Number} created on {account.Created.ToString("dd/MM/yyyy")}");
+            account.Created = new DateTime(2019,10,24);
+            Console.WriteLine($"Client {account.Owner} has {account.Balance} rubles in account number {account.Number} created on {account.Created.ToString("dd/MM/yyyy")}");
         }
     }
 }
